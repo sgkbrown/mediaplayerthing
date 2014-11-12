3e1e0916a8448ff13f80a613b3b918321fc1f819
Nutech Systems <nutechsystems@Michaels-MacBook-Air.local>
2014-11-12 10:40:59 -0500

    Initial Commit

---
+++ MediaPlayerThing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MediaPlayerThing
+//
+//  Created by Nutech Systems on 11/12/14.
+//  Copyright (c) 2014 NuTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
